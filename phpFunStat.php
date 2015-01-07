92948b1e4a43146f34ad1499fd4654df50b2aca6
oowuyue <oowuyue@gmail.com>
2015-01-07 09:14:53 +0800

    Update phpFunStat.php

--- phpFunStat.php
+++ phpFunStat.php
@@ -2,9 +2,12 @@
 
 $fArray = array();
 
+$total = 0;
+
 function readFileFromDir($dir)
 {
-    if (!is_dir($dir))
+    if (!is_dir(
+                    $dir))
         return false;
 
     $handle = opendir($dir); //打开目录
@@ -25,23 +28,26 @@ function readFileFromDir($dir)
             readFileFromDir($file); //递归查询
         }
     }
-    closedir($dir); //关闭目录
+    closedir($dir)
+
+    ; //关闭目录
 }
 
 function staticFunc($file)
 {
-    global $fArray;
+    global $fArray, $total;
     $handle = @fopen("$file", "r");
     if ($handle)
     {
-        while (($buffer = fgets($handle, 4096)) !== false)
+        while (( $buffer = fgets($handle, 4096)) !== false)
         {
-            preg_match_all("#(\w+?)\(#is", $buffer, $matches);//匹配php函数 function_name(
+            preg_match_all("#(\w+?)\(#is", $buffer, $matches); //匹配php函数 function_name(
             $functions = $matches[1];
             foreach ($functions as $fName)
             {
                 if (function_exists($fName))
                 {
+                    $total++;
                     isset($fArray["$fName"]) ? $fArray["$fName"] ++ : $fArray["$fName"] = 1;
                 }
             }
@@ -54,6 +60,42 @@ $dir = '.';
 readFileFromDir($dir);
 
 arsort($fArray);
-echo '<pre>';
-print_r($fArray);
-echo '</pre>';
+
+
+echo<<<EOD
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+  <meta charset="utf-8">
+  <link href="http://cdn.bootcss.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<table class="table table-condensed">
+    <tr>
+        <th>functionName</th>
+        <th>count</th>
+        <th>rate</th>
+    </tr>
+
+EOD;
+
+
+foreach ($fArray as $fName => $count)
+{
+    $rate = $count / $total * 100 . "%";
+    echo<<<EOD
+    <tr>
+        <td>$fName</td>
+        <td>$count</td>
+        <td>$rate</td>
+    </tr>
+EOD;
+}
+
+
+echo<<<EOD
+</table> 
+</body>
+</html>
+EOD;
