dabb534fcd82d4796bd2cdd985baa0581116805e
oowuyue <oowuyue@gmail.com>
2015-02-27 11:54:16 +0800

    Update object.js

--- object.js
+++ object.js
@@ -10,7 +10,7 @@ Foo1.prototype.get_secret = function() {
 var foo1 = new Foo1();
 
 console.log(foo1.get_secret());
-console.log(foo1.secret);
+console.log(foo1.secret); //ok
 foo1.set_secret('a new secret');
 console.log(foo1.get_secret());
 console.log('--------------------------------------------------');
@@ -29,15 +29,30 @@ var foo2 = (function() {
 }());//定义一个函数并立刻调用
 
 console.log(foo2.get_secret());
-console.log(foo2.secret);
+console.log(foo2.secret); // fale
 foo2.set_secret('a new secret');
 console.log(foo2.get_secret());
 console.log('--------------------------------------------------');
 
 /**************************************************************************/
-
-
 var foo3 = (function() {
+    this.secret = 'default'; //windos
+    console.log(this);
+    return {
+        get_secret: function() {
+            console.log(this); // return的{} 谁调用函数谁就是函数里的this
+            return this.secret;
+
+        },
+        set_secret: function(new_secret) {
+            console.log(this);
+            this.secret = new_secret;
+        }
+    };
+})();
+
+/*
+var Foo3 = function() {
     this.secret = 'default';
     console.log(this);
     return {
@@ -51,35 +66,12 @@ var foo3 = (function() {
             this.secret = new_secret;
         }
     };
-})();
-
-//var Foo3 = function() {
-//    this.secret = 'default';
-//    console.log(this);
-//    return {
-//        get_secret: function() {
-//            console.log(this);
-//            return this.secret;
-//
-//        },
-//        set_secret: function(new_secret) {
-//            console.log(this);
-//            this.secret = new_secret;
-//        }
-//    };
-//};
-//var foo3 = new Foo3();
-
+};
+var foo3 = new Foo3();
+*/
 
 console.log(foo3.get_secret());
 console.log(foo3.secret);
 foo3.set_secret('a new secret');
 console.log(foo3.get_secret());
 console.log('--------------------------------------------------');
-
-
-
-// http://www.zhihu.com/question/20292224
-// http://www.zhihu.com/question/20348948
-// http://www.zhihu.com/question/19554716
-// http://www.zhihu.com/question/19636194/answer/12504495
