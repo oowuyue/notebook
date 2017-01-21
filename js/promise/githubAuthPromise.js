2f19c269bf5035efc4cf3140e965bfa5bf17b271
oowuyue <oowuyue@gmail.com>
2017-01-21 20:32:34 +0800

    Create githubAuthPromise.js

---
+++ js/promise/githubAuthPromise.js
@@ -0,0 +1,110 @@
+var http = require("http");
+var https = require("https");
+var url = require('url');
+var querystring = require('querystring');
+var crypto = require("crypto");
+var fs = require('fs');
+
+var server = http.createServer(function (request, response) {
+    console.log(request.url);
+    var args = url.parse(request.url, true).query;
+    if (Object.hasOwnProperty.call(args, "code")) {
+        var code = args.code;
+        new Promise(function (resolve, reject) {
+            var token_url = "https://github.com/login/oauth/access_token";
+            var postdata = querystring.stringify({
+                'client_id': '662de532889de1c07b01',
+                'client_secret': 'ccd047349fafb45eca1eddb095065d234eec2f05',
+                'redirect_uri': 'http://127.0.0.1:80',
+                'grant_type': 'authorization_code',
+                'code': code
+            });
+            var options = url.parse(token_url);
+            options.method = "POST";
+            options.headers = {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                "Content-Length": postdata.length
+            };
+            var post_req = https.request(options, function (res) {
+                var body = "";
+                res.on('data', function (data) {
+                    body += data;
+                }).on('end', function () {
+                    var resp = url.parse("https://api.github.com/user?" + body, true);
+                    var token = resp.query.access_token;
+                    console.log("[getToken ok]");
+                    resolve(token);
+                });
+            });
+            post_req.on('error', (e) => {
+                console.error(e);
+                reject(e);
+            });
+            post_req.write(postdata);
+            post_req.end();
+        })
+        .then(function (token) {
+            return new Promise(function (resolve, reject) {
+                var api_url = "https://api.github.com/user?access_token=" + token;
+                var options = url.parse(api_url);
+                options.method = "GET";
+                options.headers = {
+                    'User-Agent': 'ohmy-auth'
+                };
+                var req = https.request(options, (res) => {
+                    var info = "";
+                    res.on('data', (d) => {
+                        info += d;
+                    }).on('end', function () {
+                        console.log("[getApi ok]");
+                        resolve(info);
+
+                    });
+                });
+                req.on('error', (e) => {
+                    console.error(e);
+                    reject(e);
+                });
+                req.end();
+            });
+        })
+        .then(function (info) {
+            return new Promise(function (resolve, reject) {
+                fs.writeFile('promiseapi.txt', info.toString(), (err) => {
+                    if (err)
+                        reject(err);
+                    console.log("[logApi ok]");
+                    resolve(info+" *@@@@  promiseapi.txt");
+
+                });
+            });
+        })
+        .then(function (lastData) {
+            console.info("[notifyOk]"+ lastData);
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'text/plain');
+            response.end("[notifyOk]" + lastData);
+        })
+        .catch(function (error) {
+            console.info(error);
+        });
+    } 
+    else if (Object.hasOwnProperty.call(args, "github_login")) {
+        var code_url = "https://github.com/login/oauth/authorize";
+        var state = crypto.createHash('md5').update(Math.random().toString(), 'utf-8').digest('hex');
+        var params = {
+            'client_id': "662de532889de1c07b01",
+            'redirect_uri': "http://127.0.0.1:80",
+            'response_type': "code",
+            'scope': '',
+            'state': state
+        };
+        code_url = code_url + "?" + querystring.stringify(params);
+        response.writeHead(302, {
+            'Location': code_url
+        });
+        response.end();
+    }
+});
+
+server.listen(80);
